801b7e705164617749ce417de5e49c1296d58b36
amruthakalluri <amruthakalluru@gmail.com>
2026-01-04 19:37:03 +0530

    Added day 1 and day 2 Python practice

---
+++ day 1.py
@@ -0,0 +1,37 @@
+
+student_name = "Paul"
+student_age = "13"
+student_marks = "90"
+name = student_name.upper()
+
+print("Hi everyone,\n I'm "+name+ " ,")
+print("I am "+student_age+ "years old.")
+print("I scored "+student_marks+" in my previous math exam.")
+print(student_name.islower())
+
+print("********************************************************************")
+
+from math import *
+
+a = 23
+b = 44
+print(a * b - 45)
+print(pow(2, 8))
+print(sqrt(674))
+print(a % b)
+
+print("******************************************************************")
+
+print("    /\     ")
+print("   /  \    ")
+print("  /    \   ")
+print(" /      \  ")
+print("/________\ ")
+
+print("**********************************************************************")
+print(" ")
+
+print("|````````|")
+print("|        |")
+print("|        |")
+print("|________|")
