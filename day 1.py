50bab8406329a9d8a1ea1e39d65cc6b28df680c0
amruthakalluri <amruthakalluru@gmail.com>
2026-01-09 17:47:56 +0530

    Add files via upload

---
+++ day 1.py
@@ -0,0 +1,74 @@
+#temp = float(input("Enter the temperature in celsius: "))
+#print("The converted temperature in farenhiet: ", 9/5*temp + 32)
+
+#print(" ")
+
+#num1 = int(input("Enter a number: "))
+#num2 = int(input("Enter another number: "))
+#print("The average of 2 numbers : ",(num1+num2)/2)
+
+#print(" ")
+
+print("Exercise 1.1") 
+print("******************")
+print("******************")
+print("******************")
+print("******************")
+
+print(" ")
+
+print("Exercise 1.2") 
+print("***********************")
+print("*                     *")
+print("*                     *")
+print("***********************")
+
+print(" ")
+
+print("Exercise 1.3") 
+print("*")
+print("**")
+print("***")
+print("****")
+
+print(" ")
+
+print("Exercise 1.4")
+a = int(512-282) 
+b = int(47*48+5)
+c = a/b
+print(c)
+print(" ")
+
+print("Exercise 1.5")
+numb = float(input("Enter a number: "))
+print("The square of ", numb," is: ", numb*numb, ".", sep = '')
+print(" ")
+
+print("Exercise 1.6")
+x = int(input("Enter a number: "))
+print(x,2*x,3*x,4*x,5*x, sep='---')
+print(" ")
+
+print("Exercise 1.7")
+weight = float(input("Enter the weight in kg: "))
+print("In pounds: ", weight*2.205)
+print(" ")
+
+print("Exercise 1.8")
+a = float(input("Enter the 1st number: "))
+b = float(input("Enter the 2nd number: "))
+c = float(input("Enter the 3rd number: "))
+Total = a+b+c
+average = Total/3
+print("Sum: ",Total)
+print("Average: ",average)
+print(" ")
+
+print("Exercise 1.9")
+meal_price = float(input("Enter the price of the meal: "))
+meal_tip = float(input("Enter the tip percentage: "))
+tip_percentage = meal_price * meal_tip / 100
+total_bill = tip_percentage + meal_price
+print("Tip Amount: ",tip_percentage)
+print("Toatl Amount: ",total_bill)
